0839a5549514baeb42129cd5fee68bd671688c47
uaerman <umurerman36@gmail.com>
2025-04-16 18:59:48 +0100

    feat(commentController.js): enhance getComments function to include user data in comments response for better context refactor(commentModule.js): modify getPostComments to select author field without populating to optimize query performance

--- src/controller/commentController.js
+++ src/controller/commentController.js
@@ -1,12 +1,14 @@
 import ApiResponse from "../utils/apiResponse.js";
 
 //Cache Strategies
-import { invalidateKey } from "../cache/strategies/invalidate.js";
+import multiGet from "../cache/strategies/multiGet.js";
+import {invalidateKey} from "../cache/strategies/invalidate.js";
 
 //Cache Modules
 import *as commentCacheModule from "../cache/modules/commentModule.js";
 
 //Database Modules
+import *as userDatabaseModule from "../database/modules/userModule.js";
 import *as postDatabaseModule from "../database/modules/postModule.js";
 import *as commentDatabaseModule from "../database/modules/commentModule.js";
 
@@ -35,11 +37,29 @@ export const getComments = async (req, res) => {
     try {
         const { postId } = req.params
 
-        const comments = await commentCacheModule.getPostComments(postId, commentDatabaseModule.getPostComments)
-        if (!comments) {
+        let comments = await commentCacheModule.getPostComments(postId, commentDatabaseModule.getPostComments)
+
+        if (!comments || comments.length === 0) {
             return res.status(404).json(ApiResponse.notFound("Yorum bulunamadı", null, 404))
         }
+        const users = comments.map(comment => comment.author)
 
+        const userData = await multiGet(users, "user", userDatabaseModule.getMultiUserById)
+
+        comments = comments.map(comment => {
+            const user = userData.find(u => u._id.toString() === comment.author.toString())
+            return {
+                ...comment,
+                author: {
+                    name: user.name,
+                    surname: user.surname,
+                    profile: {
+                        avatar: user.profile?.avatar
+                    }
+                }
+            }
+        })
+        console.log(comments)
         res.status(200).json(ApiResponse.success("Yorumlar başarıyla getirildi", comments, 200))
     } catch (error) {
         console.error(error)
--- src/database/modules/commentModule.js
+++ src/database/modules/commentModule.js
@@ -12,8 +12,8 @@ export const createComment = async (commentData) => {
 export const getPostComments = async (postId) => {
     try {
         return await Comment.find({post: postId})
-            .select("content createdAt")
-            .populate("author", "name surname profile.avatar")
+            .select("content createdAt author")
+            //.populate("author", "name surname profile.avatar")
             .sort({createdAt: -1})
     } catch (error) {
         console.error(error)
