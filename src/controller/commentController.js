40c7669472240bc03d770732623e5d938dd9ec25
uaerman <umurerman36@gmail.com>
2025-04-20 22:50:16 +0300

    feat(commentController.js): include user ID in comment author object to enhance comment data structure

--- src/controller/commentController.js
+++ src/controller/commentController.js
@@ -58,6 +58,7 @@ export const getComments = async (req, res) => {
             return {
                 ...comment,
                 author: {
+                    _id: user._id,
                     name: user?.name || "Bilinmiyor",
                     surname: user?.surname || "",
                     profile: {
