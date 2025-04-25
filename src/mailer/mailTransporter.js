02ce5a6fd4bad5e461301ac1dd677ebaf3ece5fc
uaerman <umurerman36@gmail.com>
2025-04-25 18:11:49 +0100

    fix(mailer): convert MAIL_SECURE environment variable to boolean for proper configuration handling

--- src/mailer/mailTransporter.js
+++ src/mailer/mailTransporter.js
@@ -3,10 +3,9 @@ dotenv.config();
 import nodemailer from 'nodemailer';
 
 export default nodemailer.createTransport({
-    service: process.env.MAIL_SERVICE,
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
-    secure: process.env.MAIL_SECURE,
+    secure: process.env.MAIL_SECURE === "true",
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASSWORD,
